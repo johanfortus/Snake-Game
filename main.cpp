3abd93c6303d6dd6d7439eb7e86f3ad86ff324f7
johanfortus <fortusjohan@gmail.com>
2025-01-18 00:55:30 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <cmath>
+using namespace std;
+
+int main() {
+    cout << "Test" << endl;
+    return 0;
+}
