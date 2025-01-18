4abc2863a1655d1ab0bbc8357bb6ec547f61d342
johanfortus <fortusjohan@gmail.com>
2025-01-18 01:13:54 -0500

    Installed SFML

--- main.cpp
+++ main.cpp
@@ -3,9 +3,28 @@
 #include <string>
 #include <unordered_map>
 #include <cmath>
+#include <SFML/Graphics.hpp>
 using namespace std;
 
 int main() {
-    cout << "Test" << endl;
+    cout << "test" << endl;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
